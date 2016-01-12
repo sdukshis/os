e44e22870b37099ae0e0ba21fdfa9aeba84a9cc5
Pavel Filonov <pfilonov@ptsecurity.ru>
2016-01-12 12:50:10 +0300

    into lection. Process creation

--- slides/01_intro/src/fork.c
+++ slides/01_intro/src/fork.c
@@ -1,28 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+int main()
+{
+  pid_t pid = getpid();
+  printf("parent pid: %d\n", pid);
 
-int main() {
-    pid_t pid = getpid();
-    printf("parent pid: %d\n", pid);
+  pid = fork();
+  if (pid == -1) {
+    perror("fork");
+    exit(EXIT_FAILURE);
+  }
+  if (pid) {
+    printf("child pid: %d\n", pid);
+  }
+  else {
+    printf("child says this pid: %d\n", getpid());
+    printf("child says this parent pid: %d\n", getppid());
+    exit(EXIT_SUCCESS);
+  }
 
-    pid = fork();
-    if (pid == -1) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-    if (pid) {
-        printf("child pid: %d\n", pid);
-    } else {
-        printf("child says this pid: %d\n", getpid());
-        printf("child says this parent pid: %d\n", getppid());
-        exit(EXIT_SUCCESS);
-    }
-
-    int status = 0;
-    pid = wait(&status);
-    printf("process %d finished with exit code: %d\n", pid, status);
-    return 0;
+  int status = 0;
+  pid = wait(&status);
+  printf("process %d finished with exit code: %d\n", pid, status);
+  return 0;
 }
--- slides/01_intro/src/printpid.c
+++ slides/01_intro/src/printpid.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main() {
-    pid_t pid = getpid();
-    printf("%d\n", pid);
+int main()
+{
+  pid_t pid = getpid();
+  printf("%d\n", pid);
 
-    return 0;
+  return 0;
 }
